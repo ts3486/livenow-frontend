58ae3ec6ed225fea5c6c65309e8ff9441ed56112
ts3486 <taos3486@gmail.com>
2022-10-09 13:53:24 +0900

    added redux with redux toolkit and next-redux-wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app';
+import { wrapper } from '../store/store';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default MyApp;
+export default wrapper.withRedux(MyApp);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <title>About - My Clothing Store</title>
+          <meta name='description' content='The story behind My Clothing Store!' />
+          <meta property='og:title' content='About - My Clothing Store' />
+          <meta property='og:description' content='The story behind My Clothing Store!' />
+          <meta property='og:url' content='https://myclothingstore.com/about' />
+          <meta property='og:type' content='website' />
+          <link rel='icon' href='/favicon.ico' />
+          <h1>ボイラープレート</h1>
+        </Head>
         <body>
           <Main />
           <NextScript />
