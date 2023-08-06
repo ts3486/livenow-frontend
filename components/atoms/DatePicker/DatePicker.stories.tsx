39d939cfe8b20e82b09d22707f15a60f29bb6bcd
ts3486 <taos3486@gmail.com>
2023-08-06 18:26:40 +0900

    datePickerコメントアウト

--- components/atoms/DatePicker/DatePicker.stories.tsx
+++ components/atoms/DatePicker/DatePicker.stories.tsx
@@ -1,26 +1,26 @@
-//TextField.stories.ts|tsx
+// //TextField.stories.ts|tsx
 
-import React from 'react';
+// import React from 'react';
 
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import TextField from './DatePicker';
+// import TextField from './DatePicker';
 
-//👇 This default export determines where your story goes in the story list
-export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
-  title: 'Atoms/DatePicker',
-  component: TextField,
-} as ComponentMeta<typeof TextField>;
+// //👇 This default export determines where your story goes in the story list
+// export default {
+//   /* 👇 The title prop is optional.
+//    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+//    * to learn how to generate automatic titles
+//    */
+//   title: 'Atoms/DatePicker',
+//   component: TextField,
+// } as ComponentMeta<typeof TextField>;
 
-//👇 We create a “template” of how args map to rendering
-const Template: ComponentStory<typeof TextField> = (args) => <TextField {...args} />;
+// //👇 We create a “template” of how args map to rendering
+// const Template: ComponentStory<typeof TextField> = (args) => <TextField {...args} />;
 
-export const FirstStory = Template.bind({});
+// export const FirstStory = Template.bind({});
 
-FirstStory.args = {
-  /*👇 The args you need here will depend on your component */
-};
+// FirstStory.args = {
+//   /*👇 The args you need here will depend on your component */
+// };
--- components/atoms/DatePicker/DatePicker.tsx
+++ components/atoms/DatePicker/DatePicker.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import MuiTextField from '@material-ui/core/TextField';
 
 const TextField = (props: any) => {
-  return <MuiTextField label='Filled' variant='filled' />;
+  return ;
 };
 
 export default TextField;
