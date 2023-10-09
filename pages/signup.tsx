ccd4df9f5cf6afe1de8d6fc1934608c8a31196f8
ts3486 <taos3486@gmail.com>
2023-10-09 10:15:54 +0900

    quit FormProvider

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FormProvider, SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { passwordRules } from '@/consts/validation';
@@ -23,13 +23,12 @@ export default function Venue() {
   const {
     register,
     formState: { errors },
-  } = useForm();
-  const methods = useForm<Form>({
-    mode: 'onBlur',
+    handleSubmit,
+  } = useForm({
     defaultValues: {
-      name: 'dfafa',
-      email: 'test@gmail.com',
-      password: 'diofjaiofjaoifijaoi',
+      name: '',
+      email: '',
+      password: '',
     },
     resolver: yupResolver(schema),
   });
@@ -51,38 +50,35 @@ export default function Venue() {
           className='p-8 bg-gray-100 border-gray-100 border-2 w-1/2 flex flex-col justify-center
         '>
           <h1 className='font-bold text-3xl flex justify-center mb-16'>Signup</h1>
-          {/* MEMO: FormProvider should normally be used for nested forms but shouldn't hurt to leave it there in case of increasing forms */}
-          <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit, onErrors)} method='post' className='flex flex-col justify-center'>
-              <div className='flex flex-col justify-start mb-5'>
-                <label className='text-xl font-bold' htmlFor='name'>
-                  Your name:
-                </label>
-                <input {...register('name')} type='text' className='mt-3' placeholder='Enter your name...' />
-                {errors.name && <p className='text-red-500'>{errors.name?.message}</p>}
-              </div>
+          <form onSubmit={handleSubmit(onSubmit, onErrors)} className='flex flex-col justify-center'>
+            <div className='flex flex-col justify-start mb-5'>
+              <label className='text-xl font-bold' htmlFor='name'>
+                Your name:
+              </label>
+              <input {...register('name')} type='text' className='mt-3' placeholder='Enter your name...' />
+              {errors.name && <p className='text-red-500'>{errors.name?.message}</p>}
+            </div>
 
-              <div className='flex flex-col justify-start mb-5'>
-                <label className='text-xl font-bold' htmlFor='name'>
-                  Email:
-                </label>
-                <input {...register('email')} type='text' className='mt-3' placeholder='Enter your email...' />
-                {errors.email && <p className='text-red-500'>{errors.email?.message}</p>}
-              </div>
+            <div className='flex flex-col justify-start mb-5'>
+              <label className='text-xl font-bold' htmlFor='email'>
+                Email:
+              </label>
+              <input {...register('email')} type='text' className='mt-3' placeholder='Enter your email...' />
+              {errors.email && <p className='text-red-500'>{errors.email?.message}</p>}
+            </div>
 
-              <div className='flex flex-col justify-start mb-5'>
-                <label className='text-xl font-bold' htmlFor='name'>
-                  Password:
-                </label>
-                <input {...register('password')} type='text' className='mt-3' placeholder='Enter a strong password...' />
-                {errors.password && <p className='text-red-500'>{errors.password?.message}</p>}
-              </div>
+            <div className='flex flex-col justify-start mb-5'>
+              <label className='text-xl font-bold' htmlFor='password'>
+                Password:
+              </label>
+              <input {...register('password')} type='text' className='mt-3' placeholder='Enter a strong password...' />
+              {errors.password && <p className='text-red-500'>{errors.password?.message}</p>}
+            </div>
 
-              <div className='flex items-center justify-end border-t border-solid border-blueGray-200 rounded-b my-2'>
-                <input type='submit' />
-              </div>
-            </form>
-          </FormProvider>
+            <div className='flex items-center justify-end border-t border-solid border-blueGray-200 rounded-b my-2'>
+              <input type='submit' />
+            </div>
+          </form>
         </div>
       </section>
     </div>
